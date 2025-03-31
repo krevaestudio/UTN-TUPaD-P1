8770654d89b999aaa85ad239c8280b94f5ebc7c5
Andres Muñoz <hola@krevaestudio.com>
2025-03-30 22:05:21 -0300

    Ejemplos de condicionales en probando.py

---
+++ 03 Estructuras Condicionales/probando.py
@@ -0,0 +1,20 @@
+uno = 1
+dos = 2
+tres = 3
+cuatro = 4
+cinco = 5
+
+print(f"es {uno} menor a {cuatro}?")
+print(uno < cuatro)
+
+print(f"es {uno} mayor o igual a 1?")
+print(uno >= 1)
+
+print(f"es 2 igual a {dos}?")
+print(2 == dos)
+
+print(f"es {tres} diferente a {dos}?")
+print(tres != dos)
+
+print(f"es {cinco} menor o igual a {dos}?")
+print(cinco <= dos)
